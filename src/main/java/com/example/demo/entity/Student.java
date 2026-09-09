287ec49e9f5816f11444eed4271b7ffd269f2674
AdaL0vy <akaikyanaopro@gmail.com>
2026-09-09 18:50:17 +0300

    Feat : Add Student Repository with Jpa

---
+++ src/main/java/com/example/demo/entity/Student.java
@@ -0,0 +1,11 @@
+package com.example.demo.entity;
+
+
+import lombok.Builder;
+
+@Builder
+public class Student {
+    public String id;
+    public String firstName;
+    public String lastName;
+}
