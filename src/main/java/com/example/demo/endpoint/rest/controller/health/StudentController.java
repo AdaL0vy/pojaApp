c34373db077473d8862fdb930facb0c9d70e61b2
AdaL0vy <akaikyanaopro@gmail.com>
2026-09-09 20:45:34 +0300

    Feat : Add Mockito test and Student Controller

---
+++ src/main/java/com/example/demo/endpoint/rest/controller/health/StudentController.java
@@ -0,0 +1,24 @@
+package com.example.demo.endpoint.rest.controller.health;
+
+import com.example.demo.entity.Student;
+import com.example.demo.service.StudentService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class StudentController {
+    private StudentService studentService;
+
+    @GetMapping("/students")
+
+    public Student studentById(@PathVariable String id){
+        return studentService.getStudentById(id);
+
+    }
+
+
+
+
+}
