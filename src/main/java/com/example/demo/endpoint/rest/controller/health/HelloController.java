1d0783b50f1cb7e90bd5617db1270b5a7b70c23e
AdaL0vy <akaikyanaopro@gmail.com>
2026-09-12 18:29:53 +0300

    feat: arrange code presentation

--- src/main/java/com/example/demo/endpoint/rest/controller/health/HelloController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/health/HelloController.java
@@ -2,26 +2,25 @@ package com.example.demo.endpoint.rest.controller.health;
 
 import com.example.demo.endpoint.event.EventProducer;
 import com.example.demo.endpoint.event.model.SendEmailRequested;
+import java.util.List;
 import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 public class HelloController {
   private final EventProducer<SendEmailRequested> eventProducer;
 
-    public HelloController(EventProducer<SendEmailRequested> eventProducer) {
-        this.eventProducer = eventProducer;
-    }
+  public HelloController(EventProducer<SendEmailRequested> eventProducer) {
+    this.eventProducer = eventProducer;
+  }
 
-    @GetMapping("/hello")
-    @SneakyThrows
-    public String HelloWorld(@RequestParam String to) {
-      var event = SendEmailRequested.builder().to(to).build();
-      eventProducer.accept(List.of(event));
-      return "...World!";
+  @GetMapping("/hello")
+  @SneakyThrows
+  public String HelloWorld(@RequestParam String to) {
+    var event = SendEmailRequested.builder().to(to).build();
+    eventProducer.accept(List.of(event));
+    return "...World!";
   }
 }
