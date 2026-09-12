7bcbcb1fa36bc48eda72ee0ac1ab96d8d6540923
AdaL0vy <akaikyanaopro@gmail.com>
2026-09-12 18:24:34 +0300

    feat: add emailSender event to hello World

--- src/main/java/com/example/demo/endpoint/rest/controller/health/HelloController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/health/HelloController.java
@@ -1,12 +1,27 @@
 package com.example.demo.endpoint.rest.controller.health;
 
+import com.example.demo.endpoint.event.EventProducer;
+import com.example.demo.endpoint.event.model.SendEmailRequested;
+import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HelloController {
-  @GetMapping("/hello")
-  public String HelloWorld() {
-    return "... World!!";
+  private final EventProducer<SendEmailRequested> eventProducer;
+
+    public HelloController(EventProducer<SendEmailRequested> eventProducer) {
+        this.eventProducer = eventProducer;
+    }
+
+    @GetMapping("/hello")
+    @SneakyThrows
+    public String HelloWorld(@RequestParam String to) {
+      var event = SendEmailRequested.builder().to(to).build();
+      eventProducer.accept(List.of(event));
+      return "...World!";
   }
 }
