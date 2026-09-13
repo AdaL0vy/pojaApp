5fc817d92f8c1514db3fa7effd6e2252624a2ef0
AdaL0vy <akaikyanaopro@gmail.com>
2026-09-13 20:11:00 +0300

    feat: add bucket try

--- src/main/java/com/example/demo/endpoint/rest/controller/health/HelloController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/health/HelloController.java
@@ -2,6 +2,7 @@ package com.example.demo.endpoint.rest.controller.health;
 
 import com.example.demo.endpoint.event.EventProducer;
 import com.example.demo.endpoint.event.model.SendEmailRequested;
+import com.example.demo.service.HelloWorldService;
 import java.util.List;
 import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,16 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
   private final EventProducer<SendEmailRequested> eventProducer;
+  private final HelloWorldService service;
 
-  public HelloController(EventProducer<SendEmailRequested> eventProducer) {
+  public HelloController(
+      EventProducer<SendEmailRequested> eventProducer, HelloWorldService service) {
     this.eventProducer = eventProducer;
+    this.service = service;
   }
 
   @GetMapping("/hello")
   @SneakyThrows
-  public String HelloWorld(@RequestParam String to) {
+  public String HelloWorld(@RequestParam String to, @RequestParam String name) {
     var event = SendEmailRequested.builder().to(to).build();
     eventProducer.accept(List.of(event));
-    return "...World!";
+    return "...World!" + service.uploadHelloWorldMessage(name);
   }
 }
